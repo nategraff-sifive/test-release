75903fcbf8d18c66099c82fc1b3501bfcecc3f36
Nathaniel Graff <nathaniel.graff@sifive.com>
2020-04-29 13:29:25 -0700

    Remove markdown headings
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- scripts/create-release-notes.sh
+++ scripts/create-release-notes.sh
@@ -12,17 +12,17 @@ current_release=$1; shift 1;
 
 last_release=$(git describe --tags HEAD~)
 
-echo "# Release notes for ${project} ${current_release}"
+echo "Release notes for ${project} ${current_release}"
 
-echo "## Statistics since ${last_release}"
+echo "Statistics since ${last_release}"
 echo "- $(git rev-list --count ${last_release}..HEAD) commits"
 echo "-$(git diff --shortstat ${last_release} HEAD)"
 
 echo ""
-echo "## Authors"
+echo "Authors"
 git shortlog -s -n --no-merges ${last_release}..HEAD | cut -f 2
 
 echo ""
-echo "## Merge history"
+echo "Merge history"
 git log --merges --pretty=format:"%h %b" ${last_release}..HEAD
 
